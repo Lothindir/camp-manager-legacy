ca7792c06ca7ff9d87d2342413fdb6f896316a49
Lothindir <lothindir@gmail.com>
2019-06-20 14:21:40 +0200

    Added SVG and JS icons
    Generated by the vue-svgicon module

---
+++ src/icons/font-awesome/window-maximize.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+var icon = require('vue-svgicon')
+icon.register({
+  'font-awesome/window-maximize': {
+    width: 1792,
+    height: 1792,
+    viewBox: '0 0 1792 1792',
+    data: '<path pid="0" d="M256 1408h1280V640H256v768zM1792 288v1216q0 66-47 113t-113 47H160q-66 0-113-47T0 1504V288q0-66 47-113t113-47h1472q66 0 113 47t47 113z" _fill="#fff"/>'
+  }
+})
---
+++ src/icons/font-awesome/window-minimize.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+var icon = require('vue-svgicon')
+icon.register({
+  'font-awesome/window-minimize': {
+    width: 1792,
+    height: 1792,
+    viewBox: '0 0 1792 1792',
+    data: '<path pid="0" d="M1792 1312v192q0 66-47 113t-113 47H160q-66 0-113-47T0 1504v-192q0-66 47-113t113-47h1472q66 0 113 47t47 113z" _fill="#fff"/>'
+  }
+})
