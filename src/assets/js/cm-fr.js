71ca9aa4dbbc5a686d4f12c699a48498a00ae5e0
Lothindir <lothindir@gmail.com>
2019-06-20 14:23:01 +0200

    Added FullCalendar module
    Created custom locale to print the good allDayText

---
+++ src/assets/js/cm-fr.js
@@ -0,0 +1,21 @@
+export default {
+    code: "cm-fr",
+    week: {
+        dow: 1, // Monday is the first day of the week.
+        doy: 4  // The week that contains Jan 4th is the first week of the year.
+    },
+    buttonText: {
+        prev: "Précédent",
+        next: "Suivant",
+        today: "Courant",
+        year: "Année",
+        month: "Mois",
+        week: "Semaine",
+        day: "Jour",
+        list: "Mon Picasso"
+    },
+    weekLabel: "Sm",
+    allDayHtml: "Chef de<br/>jour",
+    eventLimitText: "en plus",
+    noEventsMessage: "Aucune activité à afficher"
+};
