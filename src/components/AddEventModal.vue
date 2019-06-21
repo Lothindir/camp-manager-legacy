c09415d18c4ab4199bcd92469d169132672fa8f9
Lothindir <lothindir@gmail.com>
2019-06-21 15:22:21 +0200

    Added modal dialog components
    * Added Modal template component
    * Added AddEventModal component for adding events

--- src/components/AddEventModal.vue
+++ src/components/AddEventModal.vue
@@ -1,54 +1,137 @@
 <template>
-    <transition name="modal">
-        <div class="modal-mask" v-show="show">
-            <div class="modal-container">
-                <div class="modal-header">
-                    <h3>New Post</h3>
-                </div>
-                <div class="modal-body">
-                    <label class="form-label">
-                        Title
-                        <input class="form-control">
+    <Modal class="z-20" v-on:close="close" v-on:submit="submit">
+        <template v-slot:title>
+            <h1>Ajouter une activité</h1>
+        </template>
+
+        <template v-slot:body>
+            <!--                <div class="form-control">-->
+            <!--                    <label class="px-1 mr-1" for="title">Titre :</label>-->
+            <!--                    <input class="form-input" type="text" name="title" id="title">-->
+            <!--                </div>-->
+            <!--                <div class="form-control">-->
+            <!--                    <label class="px-1 mr-1" for="duration">Durée :</label>-->
+            <!--                    <input class="form-input" type="time" name="duration" id="duration">-->
+            <!--                </div>-->
+            <div class="flex flex-wrap -mx-3 mb-6 w-full h-full">
+                <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                        Titre
                     </label>
-                    <label class="form-label">
-                        Body
-                        <textarea rows="5" class="form-control"></textarea>
-                    </label>
+                    <input class="form-input" id="grid-first-name" type="text" placeholder="SC 3.2 - Jeux d'eau">
                 </div>
-                <div class="modal-footer text-right">
-                    <button class="modal-default-button" @click="savePost()">
-                        Save
-                    </button>
+                <div class="w-full md:w-1/2 px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                        Durée
+                    </label>
+                    <input class="form-input" id="grid-last-name" type="time" value="01:00">
                 </div>
             </div>
-        </div>
-    </transition>
-    <!--<transition name="modal">
-        <div>
-            <h1>Ajouter une activité</h1>
-            <label for="title">Titre</label>
-            <input v-model="title" type="text" name="title" id="title">
-            <label for="duration">Durée</label>
-            <input v-model="duration" type="time" name="duration" id="duration">
-        </div>
-    </transition>-->
+            <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                        Password
+                    </label>
+                    <input class="form-input" id="grid-password" type="password" placeholder="******************">
+                    <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+                </div>
+            </div>
+            <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="eventType">
+                        Type d'activité
+                    </label>
+                    <Multiselect class="border border-gray-400 rounded text-gray-700" id="eventType" v-model="eventType" :options="eventTypes" :searchable="true" placeholder="Choisir un type d'activité">
+                    </Multiselect>
+                </div>
+            </div>
+            <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="resp">
+                        Responsable(s)
+                    </label>
+                    <Multiselect class="border border-gray-400 rounded text-gray-700" id="resp" v-model="eventManagers" :options="chiefs" :multiple="true" :searchable="false" track-by="fullName" label="code"
+                                 placeholder="Choisir au moins un responsable">
+                        <template slot="option" slot-scope="{option}">{{ option.fullName }}</template>
+                    </Multiselect>
+                </div>
+            </div>
+            <div class="flex flex-wrap -mx-3 mb-2">
+                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                        City
+                    </label>
+                    <input class="form-input" id="grid-city" type="text" placeholder="Albuquerque">
+                </div>
+                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+
+                </div>
+                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
+                        Zip
+                    </label>
+                    <input class="form-input" id="grid-zip" type="text" placeholder="90210">
+                </div>
+            </div>
+        </template>
+
+    </Modal>
 </template>
 
 <script>
+    import Modal from "../components/Modal";
+    import Multiselect from "vue-multiselect";
+
+    import "vue-multiselect/dist/vue-multiselect.min.css";
+
     export default {
         name: "AddEventModal",
-        props: ['show'],
+        components: {
+            Multiselect,
+          Modal
+        },
+        data() {
+          return {
+              eventManagers: [],
+              eventType: null,
+              chiefs: [
+                  { code: 'FM', fullName: 'Francesco Monti'},
+                  { code: 'KM', fullName: 'Katja Mosimann' },
+                  { code: 'BG', fullName: 'Benoit Guignard' }
+              ],
+              eventTypes: [
+                  "Sport de Camp",
+                  "Activité de Camp",
+                  "Vécu de Camp",
+                  "CDF",
+                  "Repas",
+                  "Social",
+                  "Divers"
+              ]
+          }
+        },
         methods: {
-            savePost: function () {
-                // Some save logic goes here...
-
+            close: function () {
                 this.$emit('close');
+            },
+            submit: function () {
+                this.$emit('submit');
             }
         }
     }
 </script>
 
 <style scoped>
+    .form-control {
+        @apply m-1 flex justify-start;
+    }
 
+    .form-input {
+        @apply appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-400 rounded py-3 px-4 mb-3 leading-tight;
+    }
+
+    .form-input:focus {
+        @apply outline-none bg-white border-gray-500;
+    }
 </style>
 
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,53 +1,25 @@
 <template>
-    <!--<div class="modal-backdrop fixed top-0 bottom-0 right-0 left-0 flex items-center align-middle">
-        <div class="modal bg-gray-300 shadow-2xl flex flex-col">
-            <slot name="header">
-                Ajouter une activité
-            </slot>
-
-            <slot name="body">
-                <label for="title">Titre</label>
-                <input type="text" name="title" id="title">
-                <label for="duration">Durée</label>
-                <input type="time" name="duration" id="duration">
-            </slot>
-
-            <slot name="footer">
-                <button v-on:click="close">Ajouter</button>
-            </slot>
-        </div>
-    </div>-->
     <transition name="modal-fade">
-        <div class="modal-backdrop">
-            <div class="modal" role="dialog" aria-labelledby="modalTitle" aria-describedby="modalDescription">
-                <header class="modal-header">
-                    <slot name="header">
-                        This is the default tile!
-
-                        <button
-                                type="button"
-                                class="btn-close"
-                                @click="close"
-                        >
-                            x
-                        </button>
+        <div class="modal-backdrop flex items-center justify-center fixed top-0 left-0 w-full h-full mt-4">
+            <div class="modal bg-gray-300 border border-gray-900 shadow-2xl flex flex-col z-10 p-2 rounded" role="dialog" aria-labelledby="modalTitle" aria-describedby="modalDescription">
+                <header class="modal-header flex items-center justify-between p-1 border-b border-gray-200 text-green-600">
+                    <slot name="title">
+                        This is the default title!
                     </slot>
+                    <div v-on:click="close">
+                        <svgicon class="w-4 h-4 fill-current text-green-600 cursor-pointer" icon="zondicons/close"></svgicon>
+                    </div>
                 </header>
-                <section class="modal-body">
+                <section class="modal-body px-2 py-1">
                     <slot name="body">
-                        I'm the default body!
+                        This is the body!
                     </slot>
                 </section>
-                <footer class="modal-footer">
+                <footer class="modal-footer flex justify-between items-center p-1 border-t border-gray-200">
                     <slot name="footer">
                         I'm the default footer!
-
-                        <button
-                                type="button"
-                                class="btn-green"
-                                @click="close"
-                        >
-                            Close me!
+                        <button type="button" class="bg-green-600 text-gray-300 rounded m-2 p-1" @click="submit">
+                            Submit!
                         </button>
                     </slot>
                 </footer>
@@ -58,58 +30,24 @@
 
 
 <script>
+    import '../icons/zondicons';
+
     export default {
         name: "Modal",
         methods: {
             close() {
                 this.$emit('close');
             },
+            submit() {
+                this.$emit('submit');
+            }
         }
     }
 </script>
 
 <style scoped>
     .modal-backdrop {
-        position: fixed;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
         background-color: rgba(0, 0, 0, 0.6);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .modal {
-        background: #FFFFFF;
-        box-shadow: 2px 2px 20px 1px;
-        overflow-x: auto;
-        display: flex;
-        flex-direction: column;
-        z-index: 10;
-    }
-
-    .modal-header,
-    .modal-footer {
-        padding: 15px;
-        display: flex;
-    }
-
-    .modal-header {
-        border-bottom: 1px solid #eeeeee;
-        color: #4AAE9B;
-        justify-content: space-between;
-    }
-
-    .modal-footer {
-        border-top: 1px solid #eeeeee;
-        justify-content: flex-end;
-    }
-
-    .modal-body {
-        position: relative;
-        padding: 20px 10px;
     }
 
     .modal-fade-enter,
@@ -119,6 +57,6 @@
 
     .modal-fade-enter-active,
     .modal-fade-leave-active {
-        transition: opacity .5s ease
+        transition: opacity .5s ease;
     }
 </style>
