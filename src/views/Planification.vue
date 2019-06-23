fc0605d9c38e7a3e0a45d2c35950d2a0d357e702
Lothindir <lothindir@gmail.com>
2019-06-23 17:17:43 +0200

    Added slot duration choice and conditional calendar editing
    * Added dropdown selection for slot duration
    * Added edit table transitions

--- src/views/Planification.vue
+++ src/views/Planification.vue
@@ -1,8 +1,25 @@
 <template>
-    <div ref="container" class="flex flex-1 items-start align-middle ml-12 px-8 pt-4 bg-gray-600 text-gray-800">
-        <div id="defaultEventsContainer" ref="defaultEventsContainer" class="border border-black bg-gray-600 mr-3 mt-20 p-1 h-auto">
-            <h1>Activités de base</h1>
-            <div v-for="defaultEvent in defaultEvents" class='draggable-event fc-event p-1 my-1' :style="getEventBackgroundColor(defaultEvent.title)" :data-event="getEventData(defaultEvent.title)" :key="defaultEvent.title">{{defaultEvent.title}}</div>
+    <div ref="container" class="flex flex-1 items-start align-middle ml-12 px-2 pt-4 bg-gray-600 text-gray-800">
+        <div id="defaultEventsContainer" ref="defaultEventsContainer" :style="this.editCalendar ? '' : 'border-color: transparent'" class="flex justify-center flex-col border border-black bg-gray-600 mr-3 mt-20 p-1 h-auto">
+            <div class="flex justify-center items-center w-32">
+                <input class="p-1" type="checkbox" name="editCheckbox" id="editCheckbox" v-model="editCalendar">
+                <label class="px-1 text-lg block uppercase tracking-wide text-gray-800 font-bold" for="editCheckbox">Modifier</label>
+            </div>
+            <transition name="edit-menu">
+                <div v-if="editCalendar">
+                    <div class="border border-black p-1 m-1">
+                        <h1 class="block uppercase tracking-wide text-gray-800 text-xs font-bold mb-2">Activités de base</h1>
+                        <div v-for="defaultEvent in defaultEvents" class='draggable-event fc-event p-1 my-1' :style="getEventBackgroundColor(defaultEvent.title)" :data-event="getEventData(defaultEvent.title)" :key="defaultEvent.title">{{defaultEvent.title}}</div>
+                    </div>
+
+                    <div class="border border-black p-1 m-1">
+                        <label class="block uppercase tracking-wide text-gray-800 text-xs font-bold mb-2" for="slotDuration">Intervalle de temps</label>
+                        <select v-model="slotDuration" name="slotDuration" id="slotDuration">
+                            <option v-for="slotDuration in slotDurations" :value="slotDuration.value" :key="slotDuration.value">{{ slotDuration.text }}</option>
+                        </select>
+                    </div>
+                </div>
+            </transition>
         </div>
         <FullCalendar id="calendar" ref="calendar"
                       v-resize="changeCalendarHeight"
@@ -14,13 +31,12 @@
                       all-day-text="Chef de jour"
                       min-time="06:00:00"
                       max-time="23:30:00"
-                      slot-label-interval="00:30:00"
                       nav-links="true"
-                      editable="true"
+                      :editable="editCalendar"
                       selectable="true"
                       droppable="true"
                       event-limit="true"
-                      slot-duration="00:15:00"
+                      :slot-duration="slotDuration"
                       force-event-duration="true"
                       v-on:select="selectionChanged"
                       v-on:dateClick="dateChanged"
@@ -94,6 +110,13 @@
                 selectedDate: null,
                 calendar: null,
                 showModal: false,
+                editCalendar: false,
+                slotDurations: [
+                    { value: "00:15:00", text: "15 minutes" },
+                    { value: "00:30:00", text: "30 minutes" },
+                    { value: "01:00:00", text: "1 heure" }
+                ],
+                slotDuration: "00:30:00",
                 defaultEvents: [
                     {
                         title: "Diane",
@@ -188,12 +211,14 @@
                 console.log('Sent resized event');
             },
             showModalDialog: function(eventInfo){
-                this.showModal = true;
-                this.$refs.addEventModal.$once('submit', () => {
-                    console.log('Modal submitted data');
-                    this.showModal = false;
-                    this.newEvent(eventInfo);
-                });
+                if(this.editCalendar === true) {
+                    this.showModal = true;
+                    this.$refs.addEventModal.$once('submit', () => {
+                        console.log('Modal submitted data');
+                        this.showModal = false;
+                        this.newEvent(eventInfo);
+                    });
+                }
             },
             clearModalDialog: function(){
                 this.showModal = false;
@@ -238,8 +263,6 @@
                     };
                 }
                 let generatedEvent = this.calendar.addEvent(event);
-                console.log(event);
-                console.log(generatedEvent);
 
                 ipcRenderer.send('async-new-event', this.normalizeEventObject(generatedEvent));
             },
@@ -282,5 +305,15 @@
 
     .draggable-event:hover {
         @apply bg-blue-400;
+    }
+
+    .edit-menu-enter,
+    .edit-menu-leave-active {
+        opacity: 0;
+    }
+
+    .edit-menu-enter-active,
+    .edit-menu-leave-active {
+        transition: opacity .5s ease;
     }
 </style>
