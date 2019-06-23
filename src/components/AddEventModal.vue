1902c19e73529206a6bb765ace03e974131cf38d
Lothindir <lothindir@gmail.com>
2019-06-23 21:11:53 +0200

    Deleted unsued form in AddEventModal

--- src/components/AddEventModal.vue
+++ src/components/AddEventModal.vue
@@ -5,23 +5,15 @@
         </template>
 
         <template v-slot:body>
-            <!--                <div class="form-control">-->
-            <!--                    <label class="px-1 mr-1" for="title">Titre :</label>-->
-            <!--                    <input class="form-input" type="text" name="title" id="title">-->
-            <!--                </div>-->
-            <!--                <div class="form-control">-->
-            <!--                    <label class="px-1 mr-1" for="duration">Durée :</label>-->
-            <!--                    <input class="form-input" type="time" name="duration" id="duration">-->
-            <!--                </div>-->
             <div class="flex flex-wrap -mx-3 mb-6 w-full h-full">
                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                    <label class="form-label" for="grid-first-name">
                         Titre
                     </label>
                     <input class="form-input" id="grid-first-name" type="text" placeholder="SC 3.2 - Jeux d'eau">
                 </div>
                 <div class="w-full md:w-1/2 px-3">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                    <label class="form-label" for="grid-last-name">
                         Durée
                     </label>
                     <input class="form-input" id="grid-last-name" type="time" value="01:00">
@@ -29,16 +21,7 @@
             </div>
             <div class="flex flex-wrap -mx-3 mb-6">
                 <div class="w-full px-3">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-                        Password
-                    </label>
-                    <input class="form-input" id="grid-password" type="password" placeholder="******************">
-                    <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
-                </div>
-            </div>
-            <div class="flex flex-wrap -mx-3 mb-6">
-                <div class="w-full px-3">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="eventType">
+                    <label class="form-label" for="eventType">
                         Type d'activité
                     </label>
                     <Multiselect class="border border-gray-400 rounded text-gray-700" id="eventType" v-model="eventType" :options="eventTypes" :searchable="true" placeholder="Choisir un type d'activité">
@@ -47,7 +30,7 @@
             </div>
             <div class="flex flex-wrap -mx-3 mb-6">
                 <div class="w-full px-3">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="resp">
+                    <label class="form-label" for="resp">
                         Responsable(s)
                     </label>
                     <Multiselect class="border border-gray-400 rounded text-gray-700" id="resp" v-model="eventManagers" :options="chiefs" :multiple="true" :searchable="false" track-by="fullName" label="code"
@@ -56,25 +39,11 @@
                     </Multiselect>
                 </div>
             </div>
-            <div class="flex flex-wrap -mx-3 mb-2">
-                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
-                        City
-                    </label>
-                    <input class="form-input" id="grid-city" type="text" placeholder="Albuquerque">
-                </div>
-                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-
-                </div>
-                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-                        Zip
-                    </label>
-                    <input class="form-input" id="grid-zip" type="text" placeholder="90210">
-                </div>
-            </div>
         </template>
 
+        <template v-slot:footer>
+            //TODO Add Button
+        </template>
     </Modal>
 </template>
 
@@ -132,6 +101,10 @@
 
     .form-input:focus {
         @apply outline-none bg-white border-gray-500;
+    }
+
+    .form-label {
+        @apply block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2;
     }
 </style>
 
