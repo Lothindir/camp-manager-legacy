3ac57349d1f57849512fca388ca731244026509a
Lothindir <lothindir@gmail.com>
2020-03-09 23:24:21 +0100

    feat: added startup page and edited webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   lintOnSave: false,
   pluginOptions: {
     electronBuilder: {
       outputDir: 'dist'
     }
+  },
+  chainWebpack: config => {
+    config
+    .plugin('html')
+    .tap(args => {
+      args[0].excludeChunks = ['app','startup']
+      return args
+    })
+    config
+      .plugin('html-startup')
+      .use(HtmlWebpackPlugin, [{  // Also generate a test.html
+        filename: 'startup.html',
+        title: 'Welcome to Camp Manager',
+        template: 'public/startup.html',
+        inject: true,
+        excludeChunks: ['app','main']
+      }])
   }
 }
